e1ed48a6c20c0c4e2df236e5f65a824f5f3deb08
Kendall Jackson <kendallkj00@gmail.com>
2020-12-03 02:03:05 -0500

    fix: remove old styles

--- pages/index.vue
+++ pages/index.vue
@@ -118,14 +118,6 @@ export default {}
 </script>
 
 <style>
-/* .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-} */
 .prose a {
     color: #f472b5;
     font-weight: 500;
